f019e6bfb10c14ac4a2b609b33e9e9a51f20c939
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 16:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,4 +37,7 @@ function sayHiToGrandma(string) {
   else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
