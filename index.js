8dc83fc07234defbdce33ddec058cc52576a7181
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 15:54:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toEqual()
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
